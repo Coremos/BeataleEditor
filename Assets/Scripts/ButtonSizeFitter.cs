789aeb6882378987b3c1ac261f57efdd298db55b
Coremos <coremos@naver.com>
2022-01-05 08:03:32 +0900

    ButtonSizeFitter.cs 추가
    버튼에서 RectTransform의 크기를 취하여 하위 텍스트의 PrefferedSize를 구하고 여백을 합쳐 적용하도록 제작했다. Horizontal만 적용했다.

---
+++ Assets/Scripts/ButtonSizeFitter.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+[RequireComponent(typeof(RectTransform))]
+public class ButtonSizeFitter : MonoBehaviour
+{
+    public enum FitMode { Unconstrained, PrefferedSize };
+    public FitMode HorizontalFit;
+    public FitMode VerticalFit;
+    public float HorizontalPadding;
+    public float VerticalPadding;
+
+    private RectTransform rectTransform;
+    private RectTransform RectTransform
+    {
+        get
+        {
+            if (rectTransform == null) rectTransform = GetComponent<RectTransform>();
+            return rectTransform;
+        }
+    }
+
+    private Text text;
+    private Text Text
+    {
+        get
+        {
+            if (text == null) text = GetComponentInChildren<Text>();
+            return text;
+        }
+    }
+
+    private RectTransform textRect;
+    private RectTransform TextRect
+    {
+        get
+        {
+            if (textRect == null) textRect = Text.GetComponent<RectTransform>();
+            return textRect;
+        }
+    }
+
+    private void OnValidate()
+    {
+        EditorApplication.delayCall += _OnValidate;
+    }
+
+    private void _OnValidate()
+    {
+        if (this == null) return;
+        if (HorizontalFit == FitMode.PrefferedSize) UpdateFitting(RectTransform.Axis.Horizontal);
+    }
+
+    private void UpdateFitting(RectTransform.Axis axis)
+    {
+        //RectTransform.sizeDelta = new Vector2(Text.preferredWidth, RectTransform.sizeDelta.y);
+        RectTransform.SetSizeWithCurrentAnchors(axis, Mathf.Ceil(LayoutUtility.GetPreferredSize(TextRect, (int)axis) + HorizontalPadding));
+    }
+}
