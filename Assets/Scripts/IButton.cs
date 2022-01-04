c75708432fd82d0cc3ea7f0652a2e9da628574ac
Coremos <coremos@naver.com>
2022-01-05 05:06:28 +0900

    MenuItem 추가
    IButton 인터페이스 추가, MenuItem 프리팹 및 스크립트 추가

---
+++ Assets/Scripts/IButton.cs
@@ -0,0 +1,19 @@
+﻿namespace Editor
+{
+    namespace Button
+    {
+        interface IButton
+        {
+            void OnClick();
+        }
+
+        abstract class MenuButton : IButton
+        {
+            public void OnClick() { }
+
+            public virtual void OnMouseUp() { }
+            public virtual void OnMouseDown() { }
+            public virtual void OnMouseMove() { }
+        }
+    }
+}
---
+++ Assets/Scripts/MenuItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Editor
+{
+    public class MenuItem : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
