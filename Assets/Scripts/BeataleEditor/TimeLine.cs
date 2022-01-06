88517401a27db0599fa00c2ad288d039b0587e0c
Coremos <coremos@naver.com>
2022-01-06 22:24:27 +0900

    TimeLine 추가
    TimeLine에서 마우스를 올린 위치의 값으로 anchoredPosition을 설정하고 싶지만, 로컬 좌표로 지정이 되어 현재 위치 값 만큼 뺀 값으로 위치를 지정해주었다.

---
+++ Assets/Scripts/BeataleEditor/TimeLine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BeataleEditor
+{
+    class TimeLine : RectTransformEvent
+    {
+        public GameObject TimeLineObject;
+        private RectTransform timeLineRectTransform;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            timeLineRectTransform = TimeLineObject.GetComponent<RectTransform>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        public override void OnMouseEnter()
+        {
+            TimeLineObject.SetActive(true);
+        }
+        public override void OnMouseOver()
+        {
+            timeLineRectTransform.anchoredPosition = Mouse.Position - ((RectTransform)transform).anchoredPosition;
+        }
+        public override void OnMouseExit()
+        {
+            TimeLineObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/RectTransformEvent.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace BeataleEditor
+{
+    public class RectTransformEvent : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+        private bool isRectangleContainsMouse;
+
+        protected virtual void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            isRectangleContainsMouse = false;
+        }
+
+        protected virtual void Update()
+        {
+            var lastRectangleContainsMouse = isRectangleContainsMouse;
+            isRectangleContainsMouse = IsRectangleContainsMouse();
+
+            if (lastRectangleContainsMouse != isRectangleContainsMouse)
+            {
+                if (isRectangleContainsMouse) OnMouseEnter();
+                else OnMouseExit();
+            }
+
+            if (isRectangleContainsMouse) OnMouseOver();
+        }
+
+        public virtual void OnMouseOver()
+        {
+            
+        }
+
+        public virtual void OnMouseEnter()
+        {
+        }
+
+        public virtual void OnMouseExit()
+        {
+        }
+
+        public bool IsRectangleContainsMouse()
+        {
+            return IsRectangleContainsMouseHorizontal() && IsRectangleConatinsMouseVertical();
+        }
+
+        public bool IsRectangleContainsMouseHorizontal()
+        {
+            return (Mouse.Position.x >= rectTransform.anchoredPosition.x && 
+                Mouse.Position.x <= rectTransform.anchoredPosition.x + rectTransform.sizeDelta.x);
+        }
+
+        public bool IsRectangleConatinsMouseVertical()
+        {
+            return (Mouse.Position.y <= rectTransform.anchoredPosition.y &&
+                Mouse.Position.y >= rectTransform.anchoredPosition.y - rectTransform.sizeDelta.y);
+        }
+    }
+}
