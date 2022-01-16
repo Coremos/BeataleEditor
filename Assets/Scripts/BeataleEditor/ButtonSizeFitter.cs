e4f842404c7dd3c2ccf7a5486e6060637dfd4a5f
Coremos <coremos@naver.com>
2022-01-16 19:59:30 +0900

    TimeLine 경로 이동
    EditManager내의 TimeLine폴더로 경로 이동

--- Assets/Scripts/BeataleEditor/ButtonSizeFitter.cs
+++ Assets/Scripts/BeataleEditor/ButtonSizeFitter.cs
@@ -1,65 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-[ExecuteAlways]
-[RequireComponent(typeof(RectTransform))]
-public class ButtonSizeFitter : EditorBehaviour
+namespace BeataleEditor
 {
-    public enum FitMode { Unconstrained, PrefferedSize };
-    public FitMode HorizontalFit;
-    public FitMode VerticalFit;
-    public float HorizontalPadding;
-    public float VerticalPadding;
-
-    private RectTransform rectTransform;
-    private RectTransform RectTransform
+    [ExecuteAlways]
+    [RequireComponent(typeof(RectTransform))]
+    public class ButtonSizeFitter : EditorBehaviour
     {
-        get
-        {
-            if (rectTransform == null) rectTransform = GetComponent<RectTransform>();
-            return rectTransform;
-        }
-    }
+        public enum FitMode { Unconstrained, PrefferedSize };
+        public FitMode HorizontalFit;
+        public FitMode VerticalFit;
+        public float HorizontalPadding;
+        public float VerticalPadding;
 
-    private Text text;
-    private Text Text
-    {
-        get
+        private RectTransform rectTransform;
+        private RectTransform RectTransform
         {
-            if (text == null) text = GetComponentInChildren<Text>();
-            return text;
+            get
+            {
+                if (rectTransform == null) rectTransform = GetComponent<RectTransform>();
+                return rectTransform;
+            }
         }
-    }
 
-    private RectTransform textRect;
-    private RectTransform TextRect
-    {
-        get
+        private Text text;
+        private Text Text
         {
-            if (textRect == null) textRect = Text.GetComponent<RectTransform>();
-            return textRect;
+            get
+            {
+                if (text == null) text = GetComponentInChildren<Text>();
+                return text;
+            }
         }
-    }
+
+        private RectTransform textRect;
+        private RectTransform TextRect
+        {
+            get
+            {
+                if (textRect == null) textRect = Text.GetComponent<RectTransform>();
+                return textRect;
+            }
+        }
 
 #if UNITY_EDITOR
-    private void OnValidate()
-    {
-        EditorApplication.delayCall += _OnValidate;
-    }
+        private void OnValidate()
+        {
+            EditorApplication.delayCall += _OnValidate;
+        }
 #endif
 
-    private void _OnValidate()
-    {
-        if (this == null) return;
-        if (HorizontalFit == FitMode.PrefferedSize) UpdateFitting(RectTransform.Axis.Horizontal);
+        private void _OnValidate()
+        {
+            if (this == null) return;
+            if (HorizontalFit == FitMode.PrefferedSize) UpdateFitting(RectTransform.Axis.Horizontal);
+        }
+
+        private void UpdateFitting(RectTransform.Axis axis)
+        {
+            //RectTransform.sizeDelta = new Vector2(Text.preferredWidth, RectTransform.sizeDelta.y);
+            RectTransform.SetSizeWithCurrentAnchors(axis, Mathf.Ceil(LayoutUtility.GetPreferredSize(TextRect, (int)axis) + HorizontalPadding));
+        }
     }
 
-    private void UpdateFitting(RectTransform.Axis axis)
-    {
-        //RectTransform.sizeDelta = new Vector2(Text.preferredWidth, RectTransform.sizeDelta.y);
-        RectTransform.SetSizeWithCurrentAnchors(axis, Mathf.Ceil(LayoutUtility.GetPreferredSize(TextRect, (int)axis) + HorizontalPadding));
-    }
 }
--- Assets/Scripts/BeataleEditor/MenuItem.cs
+++ Assets/Scripts/BeataleEditor/MenuItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Editor
+namespace BeataleEditor
 {
     [RequireComponent(typeof(RectTransform))]
     public class MenuItem : MonoBehaviour
