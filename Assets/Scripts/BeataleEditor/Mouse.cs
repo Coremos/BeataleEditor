725cd19a6b31eebdd1796e8bbd85cec778fc0cad
Coremos <coremos@naver.com>
2022-01-16 15:18:17 +0900

    Mouse.cs 업데이트
    마우스 커서의 텍스쳐를 변경할 수 있도록 코드 작성

--- Assets/Scripts/BeataleEditor/Mouse.cs
+++ Assets/Scripts/BeataleEditor/Mouse.cs
@@ -1,24 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace BeataleEditor
 {
     class Mouse : MonoBehaviour
     {
+        public static Dictionary<CursorType, Texture2D> CursorTextures;
+        public enum CursorType { Normal = 0, Work, Horizontal, Vertical, Count }
         public static Vector2 Position;
         public static Vector2 DeltaPosition;
 
-        private RectTransform rectTransform;
+        private static readonly string cursorTexturePath = "Textures/Cursor/";
+        private RectTransform canvasRectTransform;
 
         private void Awake()
         {
-            rectTransform = GetComponent<RectTransform>();
+            canvasRectTransform = GetComponent<RectTransform>();
+            InitializeCursorTextures();
+        }
+
+        private void InitializeCursorTextures()
+        {
+            CursorTextures = new Dictionary<CursorType, Texture2D>();
+            for (int i = 0; i < (int)CursorType.Count; i++)
+            {
+                CursorTextures.Add((CursorType)i, Resources.Load<Texture2D>(cursorTexturePath + (CursorType)i));
+            }
         }
 
         private void Update()
         {
             var lastPosition = Position;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, null, out Position);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, Input.mousePosition, null, out Position);
             DeltaPosition = Position - DeltaPosition;
+        }
+
+        public static void SetCursor(CursorType type)
+        {
+            Cursor.SetCursor(CursorTextures[type], Vector2.zero, CursorMode.ForceSoftware);
         }
     }
 
