1c1624c3015821e59d2658728bf33c679c8fed84
Coremos <coremos@naver.com>
2022-01-31 04:59:08 +0900

    RouteSpline.cs 수정
    각각 곡선의 가속도 구하는 메소드 GetVelocity를 구현 (미분)
    외에 식 간단화

--- Assets/Scripts/Beatale/Route/RouteSpline.cs
+++ Assets/Scripts/Beatale/Route/RouteSpline.cs
@@ -50,11 +50,10 @@ namespace Beatale.Route
         public void DrawCurve(RouteVertex vertex1, RouteVertex vertex2)
         {
             var currentPoint = vertex1.Position;
-            var nextPoint = Vector3.zero;
             var resolutionStep = 1.0f / Resolution;
             for (int index = 1; index <= Resolution; index++)
             {
-                nextPoint = GetPoint(vertex1, vertex2, resolutionStep * index);
+                var nextPoint = GetPoint(vertex1, vertex2, resolutionStep * index);
                 Gizmos.DrawLine(currentPoint, nextPoint);
                 currentPoint = nextPoint;
             }
@@ -65,42 +64,19 @@ namespace Beatale.Route
             if (vertex1.Direction2 == Vector3.zero)
             {
                 if (vertex2.Direction1 == Vector3.zero) return Line.GetPoint(vertex1.Position, vertex2.Position, t);
-                else return GetQuadraticCurvePoint(vertex1.Position, vertex2.GlobalDirection1, vertex2.Position, t);
+                else return QuadraticCurve.GetPoint(vertex1.Position, vertex2.GlobalDirection1, vertex2.Position, t);
             }
             else
             {
-                if (vertex2.Direction1 == Vector3.zero) return GetQuadraticCurvePoint(vertex1.Position, vertex1.GlobalDirection2, vertex2.Position, t);
-                else return CubicBezierCurve.GetPoint(vertex1.Position, vertex1.GlobalDirection2, vertex2.GlobalDirection1, vertex2.Position, t);
+                if (vertex2.Direction1 == Vector3.zero) return QuadraticCurve.GetPoint(vertex1.Position, vertex1.GlobalDirection2, vertex2.Position, t);
+                else return CubicCurve.GetPoint(vertex1.Position, vertex1.GlobalDirection2, vertex2.GlobalDirection1, vertex2.Position, t);
             }
         }
 
         
-
-        public Vector3 GetCubicCurvePoint(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4, float t)
-        {
-            t = Mathf.Clamp01(t);
-
-            Vector3 part1 = Mathf.Pow(1 - t, 3) * p1;
-            Vector3 part2 = 3 * Mathf.Pow(1 - t, 2) * t * p2;
-            Vector3 part3 = 3 * (1 - t) * Mathf.Pow(t, 2) * p3;
-            Vector3 part4 = Mathf.Pow(t, 3) * p4;
-
-            return part1 + part2 + part3 + part4;
-        }
-
-        public Vector3 GetQuadraticCurvePoint(Vector3 p1, Vector3 p2, Vector3 p3, float t)
-        {
-            t = Mathf.Clamp01(t);
-
-            Vector3 part1 = Mathf.Pow(1 - t, 2) * p1;
-            Vector3 part2 = 2 * (1 - t) * t * p2;
-            Vector3 part3 = Mathf.Pow(t, 2) * p3;
-
-            return part1 + part2 + part3;
-        }
     }
 
-    public class CubicBezierCurve
+    public class CubicCurve
     {
         
         public float GetLength()
@@ -113,16 +89,24 @@ namespace Beatale.Route
             return 0;
         }
 
-        public static Vector3 GetPoint(Vector3 position1, Vector3 direction1, Vector3 direction2, Vector3 poisition2, float t)
+        public static Vector3 GetVelocity(Vector3 position1, Vector3 direction1, Vector3 direction2, Vector3 position2, float t)
         {
             float subT = 1.0f - t;
-            float subT2 = subT * subT;
-            Vector3 part1 = Mathf.Pow(1 - t, 3) * position1;
-            Vector3 part2 = 3 * Mathf.Pow(1 - t, 2) * t * direction1;
-            Vector3 part3 = 3 * (1 - t) * Mathf.Pow(t, 2) * direction2;
-            Vector3 part4 = Mathf.Pow(t, 3) * poisition2;
 
-            return subT * subT * subT * position1;
+            return -subT * subT * position1 +
+                (3 * subT * subT - 2 * subT) * direction1 +
+                (-3 * t * t + 2 * t) * direction2 +
+                t * t * position2;
+        }
+
+        public static Vector3 GetPoint(Vector3 position1, Vector3 direction1, Vector3 direction2, Vector3 position2, float t)
+        {
+            float subT = 1.0f - t;
+
+            return subT * subT * subT * position1 +
+                3 * subT * subT * t * direction1 +
+                3 * subT * t * t * direction2 +
+                t * t * t * position2;
         }
 
         public static float GetApproximateLength(RouteVertex vertex1, RouteVertex vertex2, int resolution)
@@ -145,6 +129,26 @@ namespace Beatale.Route
         public static Vector3 GetPoint(Vector3 vertex1, Vector3 vertex2, float t)
         {
             return vertex1 + ((vertex2 - vertex1) * t);
+        }
+    }
+
+    public class QuadraticCurve
+    {
+        public static Vector3 GetVelocity(Vector3 p1, Vector3 p2, Vector3 p3, float t)
+        {
+            float subT = 1.0f - t;
+            return 2 * subT * p1 +
+                (2 - 4 * t) * p2 +
+                2 * t * p3;
+        }
+
+        public static Vector3 GetPoint(Vector3 p1, Vector3 p2, Vector3 p3, float t)
+        {
+            float subT = 1.0f - t;
+
+            return subT * subT * p1 +
+                2 * subT * t * p2 +
+                t * t * p3;
         }
     }
 }
