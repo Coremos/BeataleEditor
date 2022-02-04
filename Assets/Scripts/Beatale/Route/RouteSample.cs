1af15666b6a3254ce97f13f8edfda0ef6dd0b750
Coremos <coremos@naver.com>
2022-02-04 23:50:03 +0900

    RouteSample.cs 수정
    Lerp 메소드 생성

--- Assets/Scripts/Beatale/Route/RouteSample.cs
+++ Assets/Scripts/Beatale/Route/RouteSample.cs
@@ -15,6 +15,15 @@ namespace Beatale.Route
             Up = up;
         }
 
+        public static RouteSample Lerp(RouteSample routeSample1, RouteSample routeSample2, float t)
+        {
+            return new RouteSample(
+                Vector3.Lerp(routeSample1.Position, routeSample2.Position, t),
+                Vector3.Lerp(routeSample1.Direction, routeSample2.Direction, t).normalized,
+                Vector3.Lerp(routeSample1.Up, routeSample2.Up, t)
+                );
+        }
+
         public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType()) return false;
