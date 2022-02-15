b68d0098d5912e240fc4e47e2cb9d7162470ed34
Coremos <coremos@naver.com>
2022-02-15 23:57:37 +0900

    TunnelMesh.cs 수정
    주석 제거

--- Assets/Scripts/Beatale/Tunnel/TunnelMeshBender.cs
+++ Assets/Scripts/Beatale/Tunnel/TunnelMeshBender.cs
@@ -31,27 +31,14 @@ namespace Beatale.TunnelSystem
             if (Input.GetKeyDown(KeyCode.A)) isMove = true;
             if (Input.GetKey(KeyCode.RightArrow)) Roll += 5f;
             if (Input.GetKey(KeyCode.LeftArrow)) Roll -= 5f;
-            //if (Input.GetKey(KeyCode.UpArrow)) Debug.Log(Quaternion.AngleAxis(Roll, Vector3.forward) * Vector3.up);
             if (Input.GetKey(KeyCode.UpArrow)) Debug.Log(Quaternion.FromToRotation(Vector3.zero, TestVector3.normalized - Vector3.forward) * Vector3.up);
-            //Tunnel.transform.position = Quaternion.AngleAxis(90.0f, routeSamples[currentIndex].Direction) * routeSamples[currentIndex].Direction;
-            //var upVector = Quaternion.FromToRotation(Vector3.zero, TestVector3.normalized - Vector3.forward) * Vector3.up;
 
-            //var upVector = Quaternion.FromToRotation(Vector3.zero, TestVector3.normalized - Vector3.forward) * Vector3.up;
-            
-            //var upVector = Vector3.Cross(Vector3.Cross(TestVector3, Vector3.up), TestVector3).normalized;
-            //Debug.DrawRay(Tunnel.transform.position, TestVector3, Color.red);
-            //Debug.DrawRay(Tunnel.transform.position, upVector);
-            //Debug.Log(upVector);
-            
-            //Tunnel.transform.rotation = Quaternion.LookRotation(TestVector3.normalized, upVector);
             if (!isMove) return;
 
             if (currentIndex < routeSamples.Count)
             {
                 Tunnel.transform.position = Vector3.MoveTowards(Tunnel.transform.position, routeSamples[currentIndex].Position, Speed * Time.deltaTime);
                 Tunnel.transform.rotation = Quaternion.LookRotation(routeSamples[currentIndex].Direction, routeSamples[currentIndex].Up);
-                //Tunnel.transform.rotation = Quaternion.LookRotation(routeSamples[currentIndex].Direction);
-                //Tunnel.transform.rotation = Quaternion.Euler(Quaternion.AngleAxis(90.0f, routeSamples[currentIndex].Direction) * routeSamples[currentIndex].Direction);
                 if (Vector3.Distance(Tunnel.transform.position, routeSamples[currentIndex].Position) == 0f)
                 {
                     currentIndex++;
