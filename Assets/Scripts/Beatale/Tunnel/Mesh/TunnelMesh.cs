e8251113b9d243cc29007efa245b8d1e29e71c6f
Coremos <coremos@naver.com>
2022-02-22 07:38:28 +0900

    TunnelMesh.cs 생성 및 수정
    Mesh의 속성을 호출할 때마다 엄청난 비용이 수반된다는 사실을 깨닫고 터널의 메쉬를 캐싱하여 사용할 수 있도록 클래스를 작성하였다.

---
+++ Assets/Scripts/Beatale/Tunnel/Mesh/TunnelMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beatale.Tunnel
+{
+    public struct TunnelMesh
+    {
+        public Vector3[] Vertices;
+        public Vector3[] Normals;
+        public int[] Triangles;
+        public Vector2[] UV;
+
+        public static TunnelMesh GenerateMesh(Mesh mesh)
+        {
+            var newMesh = new TunnelMesh();
+            newMesh.Vertices = mesh.vertices;
+            newMesh.Normals = mesh.normals;
+            newMesh.Triangles = mesh.triangles;
+            newMesh.UV = mesh.uv;
+
+            return newMesh;
+        }
+    }
+}
