e53f9e64859c62a268efc96654effd64e87d9e0c
Coremos <coremos@naver.com>
2022-02-25 04:04:37 +0900

    CurveSample 클래스 작성
    빠른 경로 샘플 탐색을 위해 물리적 길이와 샘플들을 가지고 있을 수 있는 클래스를 생성했다.

---
+++ Assets/Scripts/Beatale/Route/Curve/CurveSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beatale.Route.Curve
+{
+    public class CurveSample
+    {
+        public float Length;
+        public List<RouteSample> Samples;
+
+        public CurveSample()
+        {
+            Samples = new List<RouteSample>();
+        }
+
+        public RouteSample GetRouteSample(float distance)
+        {
+            for (int index = 1; index < Samples.Count; index++)
+            {
+                if (distance < Samples[index].Distance)
+                {
+                    float t = (distance - Samples[index - 1].Distance) / (Samples[index].Distance - Samples[index - 1].Distance);
+                    return RouteSample.Lerp(Samples[index - 1], Samples[index], t);
+                }
+            }
+            throw new Exception("Can't find RouteSample");
+        }
+
+        public void Add(RouteSample sample)
+        {
+            Samples.Add(sample);
+        }
+    }
+}
