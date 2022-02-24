05e912cb77a0fc21f5acb98436852ba0e4b50cbf
Coremos <coremos@naver.com>
2022-02-25 04:16:56 +0900

    TunnelNoteProjector 클래스 생성 및 수정
    TunnelManager클래스 객체에서 PlayTime, TunnelMeshBender에서 BentMesh를 가져와 노트를 터널에 영사하는 역할을 수행하도록 제작했다.

--- Assets/Scripts/Beatale/Tunnel/TunnelManager.cs
+++ Assets/Scripts/Beatale/Tunnel/TunnelManager.cs
@@ -1,44 +1,49 @@
 using Beatale.ChartSystem;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Beatale
+namespace Beatale.TunnelSystem
 {
-    namespace TunnelSystem
+    public class TunnelManager : MonoBehaviour
     {
-        public class TunnelManager : MonoBehaviour
+        public float BPM;
+        public Chart Chart;
+        public float PlayTime;
+        public MusicManager MusicManager;
+
+        private bool isMove;
+
+        private void Update()
         {
-            public float BPM;
-            public Chart Chart;
-            public float Time;
-            public MusicManager MusicManager;
+            if (Input.GetKeyDown(KeyCode.A)) isMove = !isMove;
 
-            public void GetFractionTime()
-            {
-                int position = 0, numerator = 0, denominator = 0;
-                var BPMChanges = Chart.BPMChanges.Where(x => x.BPM > 10);
-            }
+            if (!isMove) return;
+            PlayTime += Time.deltaTime;
+        }
 
-            public float CalculateFloatTime(int position, int numerator, int denominator, float bpm)
-            {
-                var oneBeatTime = 60.0f / bpm;
-                float time = position * oneBeatTime + (numerator / denominator);
-                return time;
-            }
+        public void GetFractionTime()
+        {
+            int position = 0, numerator = 0, denominator = 0;
+            var BPMChanges = Chart.BPMChanges.Where(x => x.BPM > 10);
+        }
 
-            public void CalculateFractionTime(float time, float bpm, float division, out int position, out int numerator, out int denominator)
-            {
-                var oneBeatTime = 60.0f / bpm;
-                var quntitizeTime = oneBeatTime / division;
+        public float CalculateFloatTime(int position, int numerator, int denominator, float bpm)
+        {
+            var oneBeatTime = 60.0f / bpm;
+            float time = position * oneBeatTime + (numerator / denominator);
+            return time;
+        }
 
-                var index = time / oneBeatTime; // 몇 번째인지 계산됨
-                position = (int)index;
-                var difference = index - position;
-                numerator = (int)(difference * oneBeatTime / quntitizeTime);
-                denominator = (int)division;
-            }
+        public void CalculateFractionTime(float time, float bpm, float division, out int position, out int numerator, out int denominator)
+        {
+            var oneBeatTime = 60.0f / bpm;
+            var quntitizeTime = oneBeatTime / division;
+
+            var index = time / oneBeatTime; // 몇 번째인지 계산됨
+            position = (int)index;
+            var difference = index - position;
+            numerator = (int)(difference * oneBeatTime / quntitizeTime);
+            denominator = (int)division;
         }
     }
 }
