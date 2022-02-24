aa6faab3318b1ed7f8d9fcfb473d2949dab568e0
Coremos <coremos@naver.com>
2022-02-25 04:07:17 +0900

    RouteSample 클래스 수정
    경로 샘플 내에 거리까지 저장하도록 수정, 변환된 좌표를 구하는 메소드를 생성했다.

--- Assets/Scripts/Beatale/Route/RouteSample.cs
+++ Assets/Scripts/Beatale/Route/RouteSample.cs
@@ -8,38 +8,40 @@ namespace Beatale.Route
         public Vector3 Direction;
         public Vector3 Up;
         public float Roll;
-        public Quaternion Rotation
-        {
-            get
-            {
-                return GetRotation();
-            }
-            set { Rotation = value; }
-        }
+        public Quaternion Rotation;
+        public float Distance;
 
-        public RouteSample(Vector3 position, Vector3 direction, Vector3 up, float roll, Quaternion rotation)
+        public RouteSample(Vector3 position, Vector3 direction, Vector3 up, float roll, Quaternion rotation, float distance)
         {
             Position = position;
             Direction = direction;
             Up = up;
             Roll = roll;
             Rotation = rotation;
+            Distance = distance;
+        }
+
+        public Vector3 GetBentPosition(Vector3 vector)
+        {
+            vector.z = 0;
+            return (Rotation * vector) + Position;
+        }
+
+        public Vector3 GetBentNormal(Vector3 vector)
+        {
+            return Rotation * vector;
         }
 
         public static RouteSample Lerp(RouteSample routeSample1, RouteSample routeSample2, float t)
         {
             return new RouteSample(
                 Vector3.Lerp(routeSample1.Position, routeSample2.Position, t),
-                Vector3.Lerp(routeSample1.Direction, routeSample2.Direction, t).normalized,
-                Vector3.Lerp(routeSample1.Up, routeSample2.Up, t),
+                Vector3.Lerp(routeSample1.Direction, routeSample2.Direction, t),
+                Vector3.Lerp(routeSample1.Up, routeSample2.Up, t).normalized,
                 routeSample1.Roll + (routeSample2.Roll - routeSample1.Roll) * t,
-                Quaternion.Lerp(routeSample1.Rotation, routeSample2.Rotation, t)
+                Quaternion.Lerp(routeSample1.Rotation, routeSample2.Rotation, t),
+                routeSample1.Distance + (routeSample2.Distance - routeSample1.Distance) * t
                 );
-        }
-
-        public Quaternion GetRotation()
-        {
-            return Quaternion.AngleAxis(Roll, Up);
         }
 
         public override bool Equals(object obj)
