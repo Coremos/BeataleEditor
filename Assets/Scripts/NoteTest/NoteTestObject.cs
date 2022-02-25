e1d6beac9a5f9222ddb6dfd7dad3d51a289bc548
Coremos <coremos@naver.com>
2022-02-26 06:02:01 +0900

    씬 및 노트 프리팹 업데이트
    노트의 크기를 임의로 줄이고, 여러 테스트 씬을 수정했다.

--- Assets/Scripts/NoteTest/NoteTestObject.cs
+++ Assets/Scripts/NoteTest/NoteTestObject.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class NoteTestObject : MonoBehaviour
 {
+    public Vector3 Position;
     public float Speed;
 
     void Update()
     {
-        transform.Translate(Vector3.down * Speed * Time.deltaTime);
+        //transform.Translate(Vector3.down * Speed * Time.deltaTime);
     }
 }
