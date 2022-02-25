92ac51b08a1c5b8769489bf625dc564663d11e84
Coremos <coremos@naver.com>
2022-02-26 05:54:23 +0900

    NotePool.cs 생성
    노트를 풀링하기 위한 풀을 생성했다.

---
+++ Assets/Scripts/Beatale/Chart/NotePool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beatale.ChartSystem
+{
+    public class NotePool : MonoBehaviour
+    {
+        public GameObject Prefab;
+        private Queue<GameObject> objectPool;
+
+        private void Awake()
+        {
+            objectPool = new Queue<GameObject>();
+            InstantiateObject(1000);
+        }
+
+        private void InstantiateObject(int count)
+        {
+            for(int index = 0; index < count; index++)
+            {
+                var gameObject = Instantiate(Prefab, transform);
+                gameObject.SetActive(false);
+                objectPool.Enqueue(gameObject);
+            }
+        }
+
+        public GameObject GetObject()
+        {
+            if (objectPool.Count == 0)
+            {
+                InstantiateObject(10);
+            }
+            var gameObject = objectPool.Dequeue();
+            return gameObject;
+        }
+
+        public void RestoreObject(GameObject gameObject)
+        {
+            gameObject.SetActive(false);
+            objectPool.Enqueue(gameObject);
+        }
+    }
+}
