ac9a27411d804eb40f0d52c24de676d90575e67f
Coremos <coremos@naver.com>
2022-03-20 23:44:53 +0900

    LongNoteArranger.cs 수정
    점들의 좌표를 구하는 메소드 수정.

--- Assets/Scripts/Beatale/ChartSystem/LongNoteArranger.cs
+++ Assets/Scripts/Beatale/ChartSystem/LongNoteArranger.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Beatale.ChartSystem
@@ -9,19 +10,38 @@ namespace Beatale.ChartSystem
 
         public void PreLoad()
         {
+            LongNoteSampler.LongNoteSampling(chart.LongNotes);
+
             TimeCalculator.CaculateNotesTime(chart.Notes);
 
-            var s = chart.Notes.Select(note=>note.Position);
+            var s = chart.Notes.Select(note => note.Position);
+        }
+
+        public void LoadLongNotes()
+        {
+            
         }
 
         public void Project()
         {
+            float angleStep = 360.0f / 10;
             for (int index = 0; index < chart.LongNotes.Count; index++)
             {
-                
+                var longNoteSamples = chart.LongNotes[index].LongNoteSamples;
+                var vertices = new List<Vector3>();
+                for (int sampleIndex = 0; sampleIndex < longNoteSamples.Count; sampleIndex++)
+                {
+                    var longNoteSample = longNoteSamples[sampleIndex];
+                    var halfDegree = longNoteSample.Width * 0.5f;
+                    float startAngle = longNoteSamples[sampleIndex].Degree - halfDegree;
+                    float endAngle = longNoteSamples[sampleIndex].Degree + halfDegree;
+                    for (var angle = startAngle; angle < endAngle; angle += angleStep)
+                    {
+                        var vector = new Vector3(angle, 0.0f);
+                        //vertices = 
+                    }
+                }
             }
         }
-
-        
     }
 }
