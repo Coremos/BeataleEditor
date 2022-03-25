99a7abd8cb5cf0f906c78f583a888b60cf793d09
Coremos <coremos@naver.com>
2022-03-25 23:56:29 +0900

    LongNoteArranger.cs 수정
    점 연결 메소드를 수정중에있다.

--- Assets/Scripts/Beatale/ChartSystem/LongNoteArranger.cs
+++ Assets/Scripts/Beatale/ChartSystem/LongNoteArranger.cs
@@ -16,15 +16,37 @@ namespace Beatale.ChartSystem
 
             var s = chart.Notes.Select(note => note.Position);
         }
+
         public void ConnectVertices(List<List<Vector3>> verticesList)
         {
-            for (int row = 0; row < verticesList.Count; row++)
+            var triangles = new List<int>();
+            int vertexIndex = 0;
+            for (int row = 0; row < verticesList.Count - 1; row++)
             {
                 var list = verticesList[row];
+                var nextList = verticesList[row + 1];
+                var nextIndex = 0;
                 for (int index = 0; index < list.Count; index++)
                 {
+                    if (index < list.Count - 1)
+                    {
+                        triangles.Add(vertexIndex + index);
+                        triangles.Add(vertexIndex + list.Count + nextIndex);
+                        triangles.Add(vertexIndex + index + 1);
+                    }
+                    else
+                    {
+                        if (nextIndex == nextList.Count - 1)
+                        {
 
+                        }
+                        while(nextIndex < nextList.Count)
+                        {
+
+                        }
+                    }
                 }
+                vertexIndex += list.Count;
             }
         }
 
