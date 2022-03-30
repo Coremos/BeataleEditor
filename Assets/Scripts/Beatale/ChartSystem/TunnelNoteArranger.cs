d54682bc0d24079b1bfb5ac12cd9e563ee710b5a
Coremos <coremos@naver.com>
2022-03-30 23:56:11 +0900

    TunnelNoteArranger.cs 수정
    NoteTask 관련 코드 제거.

--- Assets/Scripts/Beatale/ChartSystem/TunnelNoteArranger.cs
+++ Assets/Scripts/Beatale/ChartSystem/TunnelNoteArranger.cs
@@ -18,42 +18,16 @@ namespace Beatale.ChartSystem
         public float Interval;
 
         public float TunnelSpeed;
-        public Stack<NoteTask> NoteTasks;
 
         private float suddenOffset = 0.0f;
         private float[] radiusTable;
 
-        public class NoteTask
-        {
-            public enum TaskType { Move, Delete };
-            public TaskType Type;
-            public Note Note;
-            public Vector3 Position;
-            public Vector3 Normal;
-
-            public NoteTask(TaskType type, Note note)
-            {
-                Type = type;
-                Note = note;
-            }
-
-            public NoteTask(TaskType type, Note note, Vector3 position)
-            {
-                Type = type;
-                Note = note;
-                Position = position;
-            }
-        }
-
-        private bool isRun;
-        private bool isNeedUpdate;
         private Dictionary<float, RouteSample> routeSamples;
 
         private void Awake()
         {
             //noteSchedul = new Stack<Note>();
             routeSamples = new Dictionary<float, RouteSample>();
-            NoteTasks = new Stack<NoteTask>();
             Chart = new Chart();
             Chart.Notes = new List<Note>();
             for (int index = 0; index < 5000; index++)
@@ -68,14 +42,10 @@ namespace Beatale.ChartSystem
 
             TunnelMeshGenerator.GenerateTunnelMesh();
             radiusTable = TunnelMeshGenerator.RadiusTable;
-            //Thread thread = new Thread(ProjectNote);
-            //isRun = true;
-            //thread.Start();
         }
 
         private void LateUpdate()
         {
-            isNeedUpdate = true;
             //NoteArrange();
             Project();
         }
@@ -144,73 +114,6 @@ namespace Beatale.ChartSystem
                 }
                 Chart.Notes[index].TunnelObject.transform.position = position;
                 Chart.Notes[index].TunnelObject.SetActive(true);
-            }
-        }
-
-        private void ArrangeNote()
-        {
-            
-        }
-
-        private void ProjectNote()
-        {
-            while (isRun)
-            {
-                if (!isNeedUpdate) continue;
-                //isNeedUpdate = false;
-                routeSamples.Clear();
-                for (int index = 0; index < Chart.Notes.Count; index++)
-                {
-                    float timeDifference = Chart.Notes[index].Time - TunnelManager.PlayTime;
-                    float distance = timeDifference * TunnelSpeed * TunnelManager.BPM;
-
-                    if (distance > TunnelMeshBender.BentMesh.Length - suddenOffset) break;
-                    if (distance <= 0.0f)
-                    {
-                        var deleteTask = new NoteTask(NoteTask.TaskType.Delete, Chart.Notes[index]);
-                        NoteTasks.Push(deleteTask);
-                        continue;
-                    }
-
-                    distance += TunnelMeshBender.Distance;
-
-                    RouteSample routeSample;
-                    if (!routeSamples.TryGetValue(distance, out routeSample))
-                    {
-                        routeSample = RouteSpline.GetRouteSample(distance);
-                        routeSamples[distance] = routeSample;
-                    }
-
-                    var direction = Quaternion.AngleAxis(Chart.Notes[index].Degree, routeSample.Direction) * routeSample.Up;
-                    var position = MeshProjector.GetProjection(routeSample.Position, direction, ref TunnelMeshBender.BentMesh);
-                    if (position.Equals(Vector3.zero)) continue;
-                    if ((float.IsNaN(position.x) || float.IsNaN(position.y)) || float.IsNaN(position.z)) continue;
-                    var task = new NoteTask(NoteTask.TaskType.Move, Chart.Notes[index], position);
-                    NoteTasks.Push(task);
-                }
-            }
-        }
-
-        private void NoteArrange()
-        {
-            while (NoteTasks.Count > 0)
-            {
-                var task = NoteTasks.Pop();
-                if (task == null) continue;
-                if (task.Type.Equals(NoteTask.TaskType.Delete))
-                {
-                    if (task.Note.TunnelObject == null) continue;
-                    var gameObject = task.Note.TunnelObject;
-                    task.Note.TunnelObject = null;
-                    NotePool.RestoreObject(gameObject);
-                    continue;
-                }
-                if (task.Note.TunnelObject == null)
-                {
-                    task.Note.TunnelObject = NotePool.GetObject();
-                }
-                task.Note.TunnelObject.transform.position = task.Position;
-                task.Note.TunnelObject.SetActive(true);
             }
         }
     }
