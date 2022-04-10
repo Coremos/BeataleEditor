39102cd5f9f0121754731eeb3b29728b6dee2c1c
Coremos <coremos@naver.com>
2022-04-11 00:10:37 +0900

    LongNoteArranger 클래스 수정
    LongNoteMesh를 만들기 위한 메소드들을 추가했다.
    정점을 생성하는 메소드에서 0도에서 endAngle까지 돌렸을 때 음수의 각에서 정점이 생성되지 않는 오류가 있어서 -360도부터 정점을 생성하도록 변경했다.
    정점을 연결하는 메소드는 첨자를 더하는 순서에 오류가 있어서 정상적으로 작동하지 않았었는데, 해당 순서를 올바르게 변경해서 정상적으로 작동한다.

--- Assets/Scripts/Beatale/ChartSystem/LongNoteArranger.cs
+++ Assets/Scripts/Beatale/ChartSystem/LongNoteArranger.cs
@@ -6,18 +6,60 @@ namespace Beatale.ChartSystem
 {
     public class LongNoteArranger : MonoBehaviour
     {
-        public Chart chart;
-
-        public void PreLoad()
+        public static void Project(Chart chart)
         {
-            LongNoteSampler.LongNoteSampling(chart.LongNotes);
+            var angleStep = 360.0 / 10.0;
+            for (int index = 0; index < chart.LongNotes.Count; index++)
+            {
+                var noteMesh = new LongNoteMesh();
+                var verticesList = new List<List<AngleVertex>>();
 
-            TimeCalculator.CaculateNotesTime(chart.Notes);
+                var longNoteSamples = chart.LongNotes[index].LongNoteSamples;
+                for (int sampleIndex = 0; sampleIndex < longNoteSamples.Count; sampleIndex++)
+                {
+                    var vertices = new List<AngleVertex>();
+                    var halfDegree = longNoteSamples[sampleIndex].Width * 0.5f;
+                    float startAngle = longNoteSamples[sampleIndex].Degree - halfDegree;
+                    float endAngle = longNoteSamples[sampleIndex].Degree + halfDegree;
+                    vertices.Add(new AngleVertex((float)startAngle, longNoteSamples[sampleIndex].Time));
 
-            var s = chart.Notes.Select(note => note.Position);
+                    for (var angle = -360.0; angle < endAngle; angle += angleStep)
+                    {
+                        if (angle > startAngle)
+                        {
+                            var vertex = new AngleVertex((float)angle, longNoteSamples[sampleIndex].Time);
+
+                            vertices.Add(vertex);
+                        }
+                    }
+                    vertices.Add(new AngleVertex((float)endAngle, longNoteSamples[sampleIndex].Time));
+                    verticesList.Add(vertices);
+                }
+                
+                noteMesh.AngleVertices = GenerateAngleVertices(verticesList);
+                noteMesh.Triangles = GenerateTriangles(verticesList);
+                noteMesh.UVS = GenerateUVs(verticesList);
+
+                chart.LongNotes[index].LongNoteMesh = noteMesh;
+            }
         }
 
-        public void ConnectVertices(List<List<Vector3>> verticesList)
+        public static AngleVertex[] GenerateAngleVertices(List<List<AngleVertex>> verticesList)
+        {
+            var angleVertices = new List<AngleVertex>();
+            
+            for (int index = 0; index < verticesList.Count; index++)
+            {
+                for (int column = 0; column < verticesList[index].Count; column++)
+                {
+                    angleVertices.Add(verticesList[index][column]);
+                }
+            }
+            
+            return angleVertices.ToArray();
+        }
+
+        public static int[] GenerateTriangles(List<List<AngleVertex>> verticesList)
         {
             var triangles = new List<int>();
             int vertexIndex = 0;
@@ -40,60 +82,91 @@ namespace Beatale.ChartSystem
                         triangles.Add(vertexIndex + index);
                         triangles.Add(vertexIndex + list.Count + nextIndex);
                         triangles.Add(vertexIndex + index + 1);
-
                         if (nextIndex < nextList.Count - 1) nextIndex++;
                     }
                     else
                     {
-                        while (nextIndex < nextList.Count)
+                        while (++nextIndex < nextList.Count)
                         {
                             triangles.Add(vertexIndex + index);
                             triangles.Add(vertexIndex + list.Count + nextIndex - 1);
                             triangles.Add(vertexIndex + list.Count + nextIndex);
-                            nextIndex++;
                         }
                     }
                 }
                 vertexIndex += list.Count;
             }
+            return triangles.ToArray();
         }
 
-        public void Project()
-        {
-            float angleStep = 360.0f / 10;
-            for (int index = 0; index < chart.LongNotes.Count; index++)
-            {
-                var longNoteSamples = chart.LongNotes[index].LongNoteSamples;
-                var vertices = new List<Vector3>();
-                for (int sampleIndex = 0; sampleIndex < longNoteSamples.Count; sampleIndex++)
-                {
-                    var longNoteSample = longNoteSamples[sampleIndex];
-                    var halfDegree = longNoteSample.Width * 0.5f;
-                    float startAngle = longNoteSamples[sampleIndex].Degree - halfDegree;
-                    float endAngle = longNoteSamples[sampleIndex].Degree + halfDegree;
-                    for (var angle = startAngle; angle < endAngle; angle += angleStep)
-                    {
-                        var vector = new Vector3(angle, 0.0f);
-                        vertices.Add(vector);
-                    }
-                }
-            }
-        }
 
-        public Vector2[] CalculateUVs(List<List<Vector3>> verticesList)
+        public static Vector2[] GenerateUVs(List<List<AngleVertex>> verticesList)
         {
             var uvs = new List<Vector2>();
-            var rowStep = 1 / (verticesList.Count - 1);
+
+            var rowStart = verticesList[0][0].Time;
+            var rowStep = 1 / (verticesList[verticesList.Count - 1][0].Time - rowStart);
+
             for (int row = 0; row < verticesList.Count; row++)
             {
-                var step = 1 / (verticesList[row].Count - 1);
+                var columnStart = verticesList[row][0].Degree;
+                var columnStep = 1 / verticesList[row][verticesList[row].Count - 1].Degree - columnStart;
+                
                 for (int column = 0; column < verticesList[row].Count; column++)
                 {
-                    var uv = new Vector2(column * step, rowStep * row);
+                    var uv = new Vector2(verticesList[row][column].Degree * columnStep, (float)((verticesList[row][0].Time - rowStart) * rowStep));
                     uvs.Add(uv);
                 }
             }
+
             return uvs.ToArray();
+        }
+
+        public static void DebugAngleVerticesList(List<List<AngleVertex>> verticesList)
+        {
+            for (int index = 0; index < verticesList.Count; index++)
+            {
+                for (int column = 0; column < verticesList[index].Count; column++)
+                {
+                    Debug.Log(index + "row / " + column + "col : " + verticesList[index][column].Degree);
+                }
+            }
+        }
+
+        public static void DebugAngleVerticesListCount(List<List<AngleVertex>> verticesList)
+        {
+            var value = "";
+            var count = 0;
+            for (int index = 0; index < verticesList.Count; index++)
+            {
+                for (int column = 0; column < verticesList[index].Count; column++)
+                {
+                    value += count++ + ",";
+                }
+                value += "/";
+            }
+            Debug.Log(value);
+        }
+
+        public static void DebugLongNoteMeshes(Chart chart)
+        {
+            for (int index = 0; index < chart.LongNotes.Count; index++)
+            {
+                var value = "MeshNumber" + index + ":";
+                var count = 0;
+                var triangles = chart.LongNotes[index].LongNoteMesh.Triangles;
+                for (int vertexIndex = 0; vertexIndex < triangles.Length; vertexIndex++)
+                {
+                    value += triangles[vertexIndex];
+                    if (count++ < 2) value += ",";
+                    else
+                    {
+                        value += "/";
+                        count = 0;
+                    }
+                }
+                Debug.Log(value);
+            }
         }
     }
 }
