6b27d8790305d99a40171feccfafb2cb453a005d
Coremos <coremos@naver.com>
2022-04-11 00:16:04 +0900

    TunnelNoteArranger 클래스 수정
    현재 해당 클래스에서 테스트용 채보 생성 및 시간 계산, 초기화 등을 담당하도록 만들어두었다. 우선 통합해서 제작을 한 뒤 분리할 예정이다.
    노트 간격이 다르도록 만든 코드는 GetTime 메소드를 사용하면 노트 간격이 BPM에 따라 상이하지 않게 적용된 시간으로 동작한다.

--- Assets/Scripts/Beatale/ChartSystem/TimeCalculator.cs
+++ Assets/Scripts/Beatale/ChartSystem/TimeCalculator.cs
@@ -4,75 +4,131 @@ namespace Beatale.ChartSystem
 {
     public class TimeCalculator
     {
-        public void GetBPM(Chart chart)
+        public static void GetBPM(Chart chart)
         {
             var bpmList = new Dictionary<double, double>();
             var bpm = chart.BPM;
             var length = chart.Length + chart.Offset;
             for (int index = 0; index < chart.BPMChanges.Count; index++)
             {
-                chart.BPMChanges[index];
+                //chart.BPMChanges[index];
             }
         }
 
-        public void GetTime(Chart chart)
+        public static void CalculateBPMTime2(Chart chart)
         {
-            var lastBPM = chart.BPMChanges[0].Position;
-
-
-            for (int index = 0; index < )
-        }
-
-        public void GetFractionTime()
-        {
-            int position = 0, numerator = 0, denominator = 0;
-            //var BPMChanges = Chart.BPMChanges.Where(x => x.BPM > 10);
-        }
-
-        public static void CalculateLongNoteTime(List<LongNote> longNotes)
-        {
-            for (int index = 0; index < longNotes.Count; index++)
+            chart.BPMChanges[0].Position.Time = 0.0;
+            for (int index = 1; index < chart.BPMChanges.Count; index++)
             {
-                //longNotes[index].LongNoteSamples
+                var fractionValue = 0.0;
+                var position = chart.BPMChanges[index].Position - chart.BPMChanges[index - 1].Position;
+
+                if (position.Denominator != 0) fractionValue = (position.Numerator) / (double)position.Denominator;
+
+                chart.BPMChanges[index].Position.Time = chart.BPMChanges[index - 1].Position.Time +
+                    //(60.0 / chart.BPMChanges[index - 1].BPM) *
+                    (chart.BPMChanges[index - 1].BPM / 60.0) *
+                    (position.Bar + fractionValue);
             }
-            
         }
 
-        public static float CaculatePositionTime(List<NotePosition> positions)
+        public static void CalculateBPMTime(Chart chart)
         {
-            var bpm = 60.0f;
-            var oneBeatTime = 60.0f / bpm;
-            for (int index = 0; index < positions.Count; index++)
+            chart.BPMChanges[0].Position.Time = 0.0;
+            for (int index = 1; index < chart.BPMChanges.Count; index++)
             {
-                var position = positions[index];
-                float time = position.Bar * oneBeatTime + (position.Numerator / position.Denominator);
+                var fractionValue = 0.0;
+                var position = chart.BPMChanges[index].Position - chart.BPMChanges[index - 1].Position;
+
+                if (position.Denominator != 0) fractionValue = (position.Numerator) / (double)position.Denominator;
+
+                chart.BPMChanges[index].Position.Time = chart.BPMChanges[index - 1].Position.Time +
+                    (60.0 / chart.BPMChanges[index - 1].BPM) *
+                    (position.Bar + fractionValue);
             }
-            return 0;
         }
 
-        public static float CaculateNotesTime(List<Note> notes)
+        public static void CalculateBPMTime3(Chart chart)
         {
-            var bpm = 60.0f;
-            var oneBeatTime = 60.0f / bpm;
+            chart.BPMChanges[0].Position.Time = 0.0;
+            for (int index = 1; index < chart.BPMChanges.Count; index++)
+            {
+                var fractionValue = 0.0;
+                var position = chart.BPMChanges[index].Position - chart.BPMChanges[index - 1].Position;
+
+                if (position.Denominator != 0) fractionValue = (position.Numerator) / (double)position.Denominator;
+
+                chart.BPMChanges[index].Position.Time = chart.BPMChanges[index - 1].Position.Time +
+                    chart.BPMChanges[index - 1].BPM / 60.0 / 20.0 * 1 *
+                    (position.Bar + fractionValue);
+            }
+        }
+
+        public static void GetTime(Chart chart)
+        {
+            var bpmChanges = chart.BPMChanges;
+            var lastBPM = bpmChanges[0];
+
+            var notes = chart.Notes;
+            var longNotes = chart.LongNotes;
+            var nextBPMIndex = 1;
+            var oneBeatTime = 60.0 / lastBPM.BPM;
+
             for (int index = 0; index < notes.Count; index++)
             {
-                var position = notes[index].Position;
-                float time = position.Bar * oneBeatTime + (position.Numerator / position.Denominator);
+                while (nextBPMIndex < bpmChanges.Count)
+                {
+                    if (notes[index].Position.Bar >= bpmChanges[nextBPMIndex].Position.Bar)
+                    {
+                        if ((notes[index].Position.Bar > bpmChanges[nextBPMIndex].Position.Bar) ||
+                            (notes[index].Position.Numerator * bpmChanges[nextBPMIndex].Position.Denominator >=
+                            bpmChanges[nextBPMIndex].Position.Numerator * notes[index].Position.Denominator
+                            ))
+                        {
+                            lastBPM = bpmChanges[nextBPMIndex++];
+                            oneBeatTime = 60.0 / lastBPM.BPM;
+                            continue;
+                        }
+                    }
+                    break;
+                }
+
+                var fractionValue = 0.0;
+                var barDifference = notes[index].Position.Bar - lastBPM.Position.Bar;
+                var noteNumerator = notes[index].Position.Numerator;
+                var bpmNumerator = lastBPM.Position.Numerator;
+                var denominator = (lastBPM.Position.Denominator == 0) ? 1 : lastBPM.Position.Denominator;
+
+                if (lastBPM.Position.Denominator != notes[index].Position.Denominator)
+                {
+                    noteNumerator *= (lastBPM.Position.Denominator == 0) ? 1 : lastBPM.Position.Denominator;
+                    bpmNumerator *= (notes[index].Position.Denominator == 0) ? 1 : notes[index].Position.Denominator;
+                    denominator *= (notes[index].Position.Denominator == 0) ? 1 : notes[index].Position.Denominator;
+                }
+
+                if (denominator != 0) fractionValue = (noteNumerator - bpmNumerator) / (double)denominator;
+
+                notes[index].Position.Time = lastBPM.Position.Time +
+                    oneBeatTime * (barDifference + fractionValue);
             }
-            return 0;
+
+            for (int index = 0; index < longNotes.Count; index++)
+            {
+
+            }
         }
 
-        public static float CalculateFloatTime(NotePosition position, double bpm)
+        public static double CalculateFloatTime(NotePosition position, double bpm)
         {
             return CalculateFloatTime(position.Bar, position.Numerator, position.Denominator, bpm);
         }
 
-        public static float CalculateFloatTime(int position, int numerator, int denominator, double bpm)
+        public static double CalculateFloatTime(int position, int numerator, int denominator, double bpm)
         {
             var oneBeatTime = 60.0f / bpm;
-            float time = position * oneBeatTime + (numerator / denominator);
+            var time = position * oneBeatTime + (numerator / denominator);
             return time;
-0        }
+        }
 
         public static void CalculateFractionTime(float time, float bpm, float division, out int position, out int numerator, out int denominator)
         {
--- Assets/Scripts/Beatale/ChartSystem/TunnelNoteArranger.cs
+++ Assets/Scripts/Beatale/ChartSystem/TunnelNoteArranger.cs
@@ -15,19 +15,208 @@ namespace Beatale.ChartSystem
         public RouteSpline RouteSpline;
         public Chart Chart;
         public ObjectPool NotePool;
+        public AudioSource SoundEffectTest;
         public float Interval;
 
         public float TunnelSpeed;
+
+        public int BPMIndex;
+        public float ChartTime;
+        private bool isMove;
+        private static float speed = 1.0f;
+        private static double constant = 1.0 / 60.0 / 20.0;
+        private float timePerFrame = 0f;
 
         private float suddenOffset = 0.0f;
         private float[] radiusTable;
 
         private Dictionary<float, RouteSample> routeSamples;
 
+        public static void GetTime(Chart chart)
+        {
+            var bpmChanges = chart.BPMChanges;
+            var lastBPM = bpmChanges[0];
+
+            var notes = chart.Notes;
+            var longNotes = chart.LongNotes;
+            var nextBPMIndex = 1;
+            var oneBeatTime = 60.0 / lastBPM.BPM;
+            //var oneBeatTime = lastBPM.BPM / 60.0;
+
+            for (int index = 0; index < notes.Count; index++)
+            {
+                while (nextBPMIndex < bpmChanges.Count)
+                {
+                    if (notes[index].Position > bpmChanges[nextBPMIndex].Position)
+                    {
+                        lastBPM = bpmChanges[nextBPMIndex++];
+                        oneBeatTime = 60.0 / lastBPM.BPM;
+                        //oneBeatTime = lastBPM.BPM / 60.0;
+                        continue;
+                    }
+                    break;
+                }
+
+                var fractionValue = 0.0;
+                var position = notes[index].Position - lastBPM.Position;
+
+                if (position.Denominator != 0) fractionValue = position.Numerator / (double)position.Denominator;
+
+                notes[index].Position.Time = lastBPM.Position.Time +
+                    oneBeatTime * (position.Bar + fractionValue);
+            }
+
+            for (int index = 0; index < longNotes.Count; index++)
+            {
+
+            }
+        }
+
+        public static void GetTime2(Chart chart)
+        {
+            var bpmChanges = chart.BPMChanges;
+            var lastBPM = bpmChanges[0];
+
+            var notes = chart.Notes;
+            var longNotes = chart.LongNotes;
+            var nextBPMIndex = 1;
+            var oneBeatTime = lastBPM.BPM / 60.0;
+
+            for (int index = 0; index < notes.Count; index++)
+            {
+                while (nextBPMIndex < bpmChanges.Count)
+                {
+                    if (notes[index].Position > bpmChanges[nextBPMIndex].Position)
+                    {
+                        lastBPM = bpmChanges[nextBPMIndex++];
+                        oneBeatTime = lastBPM.BPM / 60.0;
+                        continue;
+                    }
+                    break;
+                }
+
+                var fractionValue = 0.0;
+                var position = notes[index].Position - lastBPM.Position;
+
+                if (position.Denominator != 0) fractionValue = position.Numerator / (double)position.Denominator;
+
+                notes[index].Position.Time = lastBPM.Position.Time +
+                    oneBeatTime * (position.Bar + fractionValue);
+            }
+
+            for (int index = 0; index < longNotes.Count; index++)
+            {
+
+            }
+        }
+
+        public static void GetTime3(Chart chart)
+        {
+            var bpmChanges = chart.BPMChanges;
+            var lastBPM = bpmChanges[0];
+
+            var notes = chart.Notes;
+            var longNotes = chart.LongNotes;
+            var nextBPMIndex = 1;
+            var oneBeatTime = lastBPM.BPM * constant * speed;
+
+            for (int index = 0; index < notes.Count; index++)
+            {
+                while (nextBPMIndex < bpmChanges.Count)
+                {
+                    if (notes[index].Position > bpmChanges[nextBPMIndex].Position)
+                    {
+                        lastBPM = bpmChanges[nextBPMIndex++];
+                        oneBeatTime = lastBPM.BPM * constant * speed;
+                        continue;
+                    }
+                    break;
+                }
+
+                var fractionValue = 0.0;
+                var position = notes[index].Position - lastBPM.Position;
+
+                if (position.Denominator != 0) fractionValue = position.Numerator / (double)position.Denominator;
+
+                notes[index].Position.Time = lastBPM.Position.Time +
+                    oneBeatTime * (position.Bar + fractionValue);
+            }
+
+            lastBPM = bpmChanges[0];
+            nextBPMIndex = 1;
+            oneBeatTime = lastBPM.BPM * constant * speed;
+
+            for (int longNoteIndex = 0; longNoteIndex < longNotes.Count; longNoteIndex++)
+            {
+                var vertices = longNotes[longNoteIndex].LongNoteVertices;
+                for (int vertexIndex = 0; vertexIndex < vertices.Count; vertexIndex++)
+                {
+                    while (nextBPMIndex < bpmChanges.Count)
+                    {
+                        if (vertices[vertexIndex].Position > bpmChanges[nextBPMIndex].Position)
+                        {
+                            lastBPM = bpmChanges[nextBPMIndex++];
+                            oneBeatTime = lastBPM.BPM * constant * speed;
+                            continue;
+                        }
+                        break;
+                    }
+
+                    var fractionValue = 0.0;
+                    var position = vertices[vertexIndex].Position - lastBPM.Position;
+
+                    if (position.Denominator != 0) fractionValue = position.Numerator / (double)position.Denominator;
+
+                    vertices[vertexIndex].Position.Time = lastBPM.Position.Time +
+                        oneBeatTime * (position.Bar + fractionValue);
+                }
+            }
+        }
+
         private void Awake()
         {
-            //noteSchedul = new Stack<Note>();
             routeSamples = new Dictionary<float, RouteSample>();
+            MakeChartTest();
+            TimeCalculator.CalculateBPMTime3(Chart);
+            GetTime3(Chart);
+
+            TunnelMeshGenerator.GenerateTunnelMesh();
+            radiusTable = TunnelMeshGenerator.RadiusTable;
+
+            LongNoteSampler.LongNoteSampling(Chart.LongNotes);
+            LongNoteArranger.Project(Chart);
+            //DebugChart(Chart);
+            //DebugLongNote(Chart);
+            LongNoteArranger.DebugLongNoteMeshes(Chart);
+        }
+
+        public void DebugLongNote(Chart chart)
+        {
+            for (int index = 0; index < chart.LongNotes.Count; index++)
+            {
+                var vertices = chart.LongNotes[index].LongNoteSamples;
+                for (int vertexIndex = 0; vertexIndex < vertices.Count; vertexIndex++)
+                {
+                    Debug.Log(index + " : " + vertexIndex + " / " + vertices[vertexIndex].Degree + "도 " + vertices[vertexIndex].Width);
+                }
+            }
+        }
+
+        public void DebugChart(Chart chart)
+        {
+            Debug.Log(chart.BPMChanges[0].Position.Bar + ", " + chart.BPMChanges[0].Position.Numerator + " / " + chart.BPMChanges[0].Position.Denominator);
+            for (int index = 0; index < chart.Notes.Count; index++)
+            {
+                Debug.Log(index + " : " + chart.Notes[index].Position.Bar + "번째 = " + chart.Notes[index].Position.Time);
+            }
+            for (int index = 0; index < chart.BPMChanges.Count; index++)
+            {
+                Debug.Log("BPM " + index + " : " + chart.BPMChanges[index].Position.Bar + "번째 = " + chart.BPMChanges[index].Position.Time);
+            }
+        }
+
+        private void MakeChartTest1()
+        {
             Chart = new Chart();
             Chart.Notes = new List<Note>();
             for (int index = 0; index < 5000; index++)
@@ -39,14 +228,78 @@ namespace Beatale.ChartSystem
                 note.TunnelPosition = Vector3.zero;
                 Chart.Notes.Add(note);
             }
+        }
 
-            TunnelMeshGenerator.GenerateTunnelMesh();
-            radiusTable = TunnelMeshGenerator.RadiusTable;
+        private void MakeChartTest()
+        {
+            Chart = new Chart();
+            var startBPM = new BPMChange();
+            startBPM.BPM = 174;
+            //startBPM.BPM = 153;
+            //startBPM.BPM = 240;
+            //startBPM.BPM = 60;
+
+            Chart.BPMChanges.Add(startBPM);
+            var devide = 4;
+            for (int index = 0; index < 2000; index++)
+            {
+                for (int quater = 0; quater < devide; quater++)
+                {
+                    var note = new Note();
+                    note.Position.Bar = index;
+                    note.Position.Numerator = quater;
+                    note.Position.Denominator = devide;
+                    note.Degree = (index + quater) * 10;
+                    note.TunnelObject = null;
+                    note.TunnelPosition = Vector3.zero;
+                    Chart.Notes.Add(note);
+                }
+            }
+
+            var longNote = new LongNote();
+            var vertex1 = new LongNoteVertex();
+            vertex1.Degree = 0;
+            vertex1.Width = 20;
+            vertex1.Direction2 = new Vector2(-1, 0);
+            var vertex2 = new LongNoteVertex();
+            vertex2.Degree = 0;
+            vertex2.Width = 180;
+            vertex2.Direction1 = new Vector2(-1, 0);
+            longNote.LongNoteVertices.Add(vertex1);
+            longNote.LongNoteVertices.Add(vertex2);
+            Chart.LongNotes.Add(longNote);
+
+            for (int index = 1; index < 9; index++)
+            {
+                var bpmChange = new BPMChange();
+                bpmChange.Position.Bar = (index * 8) - 1;
+                bpmChange.BPM = startBPM.BPM * (1 - (1 & index) * 0.5);//(1 << index);
+                Chart.BPMChanges.Add(bpmChange);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A)) isMove = !isMove; timePerFrame = (float)Chart.BPMChanges[BPMIndex].BPM * (float)Chart.BPMChanges[BPMIndex].BPM / 60.0f * (float)constant * speed;
+
+            if (!isMove) return;
+            if ((BPMIndex < Chart.BPMChanges.Count - 1) && (ChartTime > Chart.BPMChanges[BPMIndex + 1].Position.Time))
+            {
+                timePerFrame = (float)Chart.BPMChanges[BPMIndex].BPM * (float)Chart.BPMChanges[BPMIndex].BPM / 60.0f * (float)constant * speed;
+                BPMIndex++;
+            }
+            //ChartTime += (60.0f / (float)Chart.BPMChanges[BPMIndex].BPM) * Time.deltaTime;
+            //ChartTime += ((float)Chart.BPMChanges[BPMIndex].BPM) * Time.deltaTime;
+            //ChartTime += Time.deltaTime;
+            //ChartTime += (float)Chart.BPMChanges[BPMIndex].BPM / 60.0f * Time.deltaTime;
+            //ChartTime += (float)Chart.BPMChanges[BPMIndex].BPM / 60.0f * (float)Chart.BPMChanges[BPMIndex].BPM / 60.0f / (float)constant * speed * Time.deltaTime;
+            //ChartTime += (float)Chart.BPMChanges[BPMIndex].BPM / 60.0f * (float)Chart.BPMChanges[BPMIndex].BPM * (float)constant * speed * Time.deltaTime;
+            ChartTime += timePerFrame * Time.deltaTime;
         }
 
         private void LateUpdate()
         {
-            //NoteArrange();
+            if (!isMove) return;
             Project();
         }
 
@@ -58,32 +311,14 @@ namespace Beatale.ChartSystem
             return radiusTable[index] + (interval - index) * (radiusTable[index + 1] - radiusTable[index]);
         }
 
-        private void ProjectLongNotes()
-        {
-            routeSamples.Clear();
-            for (int index = 0; index < Chart.LongNotes.Count; index++)
-            {
-
-            }
-        }
-
-        public void ArrangeLongNote(LongNote longNote)
-        {
-            for (int index = 0; index < longNote.LongNoteVertices.Count; index++)
-            {
-                //longNote.LongNoteVertices[index];
-                
-            }
-        }
-
         private void Project()
         {
             routeSamples.Clear();
             for (int index = 0; index < Chart.Notes.Count; index++)
             {
-                float timeDifference = Chart.Notes[index].Time - TunnelManager.PlayTime;
+                //float timeDifference = (float)Chart.Notes[index].Position.Time - TunnelManager.PlayTime;
+                float timeDifference = (float)Chart.Notes[index].Position.Time - ChartTime;
                 float distance = timeDifference * TunnelSpeed * TunnelManager.BPM;
-
                 if (distance > TunnelMeshBender.BentMesh.Length - suddenOffset) break;
                 if (distance <= 0.0f)
                 {
@@ -91,12 +326,12 @@ namespace Beatale.ChartSystem
                     var gameObject = Chart.Notes[index].TunnelObject;
                     Chart.Notes[index].TunnelObject = null;
                     NotePool.RestoreObject(gameObject);
+                    SoundEffectTest.PlayOneShot(SoundEffectTest.clip);
                     continue;
                 }
 
                 var noteInterval = distance / TunnelMeshBender.BentMesh.Length;
                 distance += TunnelMeshBender.Distance;
-
                 RouteSample routeSample;
                 if (!routeSamples.TryGetValue(distance, out routeSample))
                 {
@@ -115,6 +350,12 @@ namespace Beatale.ChartSystem
                 Chart.Notes[index].TunnelObject.transform.position = position;
                 Chart.Notes[index].TunnelObject.SetActive(true);
             }
+
+            //for (int index = 0; index < Chart.LongNotes.Count; index++)
+            //{
+            //    float timeStartDifference = (float)Chart.LongNotes[index].StartTime - ChartTime;
+            //    float 
+            //}
         }
     }
 }
