26143936d1822613f294e144acddb5dc3f966472
Coremos <coremos@naver.com>
2022-04-11 19:05:48 +0900

    LongNoteObject 클래스 생성 및 수정
    LongNote의 실질적인 메쉬를 담당하는 클래스를 추가했다.

---
+++ Assets/Scripts/Beatale/ChartSystem/LongNoteObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Beatale.ChartSystem
+{
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class LongNoteObject : MonoBehaviour
+    {
+        private MeshFilter meshFilter;
+        private Mesh mesh;
+
+        private void Awake()
+        {
+            meshFilter = GetComponent<MeshFilter>();
+        }
+
+        public void InitializeMesh(LongNoteMesh longNoteMesh)
+        {
+            mesh = new Mesh();
+            mesh.vertices = longNoteMesh.Vertices;
+            mesh.triangles = longNoteMesh.Triangles;
+            mesh.uv = longNoteMesh.UVS;
+            meshFilter.sharedMesh = mesh;
+        }
+
+        public void UpdateMesh(LongNoteMesh longNoteMesh)
+        {
+            mesh.vertices = longNoteMesh.Vertices;
+
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+            mesh.RecalculateTangents();
+        }
+    }
+}
