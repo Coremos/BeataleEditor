484dcb3f832b8dc116ac8653e8ec8436c51250fb
Coremos <coremos@naver.com>
2022-04-14 23:48:12 +0900

    AdjustObejct 클래스 생성
    판정을 위한 객체를 만들기 위해 담당할 클래스를 선언 및 정의했다.

---
+++ Assets/Scripts/Beatale/AdjustSystem/AdjustObject.cs
@@ -0,0 +1,18 @@
+﻿namespace Beatale.AdjustSystem
+{
+    public abstract class AdjustObject
+    {
+
+        double Time;
+    }
+
+    public class LongNoteAdjust : AdjustObject
+    {
+
+    }
+
+    public class NoteAdjust : AdjustObject
+    {
+
+    }
+}
