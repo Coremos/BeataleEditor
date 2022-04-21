34c7d7185dfa528b8b4b293326761c15e989b061
Coremos <coremos@naver.com>
2022-04-21 22:48:23 +0900

    Adjust 관련 클래스 수정
    Adjust가 판정을 조정하는 의미에 가까워서 이름을 Judge로 변경했다.

---
+++ Assets/Scripts/Beatale/JudgeSystem/Judge.cs
@@ -0,0 +1,27 @@
+﻿using Beatale.ChartSystem;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beatale.JudgeSystem
+{
+    public class Judge : MonoBehaviour
+    {
+        static readonly float MISS_TIME = 10.0f;
+        static readonly float GOOD_TIME = 2.0f;
+        static readonly float GREAT_TIME = 0.0f;
+
+        private Queue<JudgeObject> judgeQueue;
+        private Chart chart;
+        private double currentTime;
+
+        public void OnDownInput(float degree, float width, double time)
+        {
+
+        }
+
+        public void OnInput(float degree, float width, double time)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Beatale/JudgeSystem/JudgeObject.cs
@@ -0,0 +1,20 @@
+﻿using Beatale.ChartSystem;
+
+namespace Beatale.JudgeSystem
+{
+    public abstract class JudgeObject
+    {
+        double Time;
+        Note note;
+    }
+
+    public class LongNoteJudge : JudgeObject
+    {
+
+    }
+
+    public class NoteJudge : JudgeObject
+    {
+
+    }
+}
