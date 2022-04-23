1eb8f52815fbbe7a8a687ae2665f3a5ac474522f
Coremos <coremos@naver.com>
2022-04-23 23:53:37 +0900

    TunnelNoteArranger 클래스 수정
    사용하지 않는 더미 코드들을 대량 제거 하였고, ChartTimeCalculator 클래스와 ChartIntervalCalculator 클래스의 메소드로 변경했다.

--- Assets/Scripts/Beatale/ChartSystem/TunnelNoteArranger.cs
+++ Assets/Scripts/Beatale/ChartSystem/TunnelNoteArranger.cs
@@ -24,84 +24,26 @@ namespace Beatale.ChartSystem
         public int BPMIndex;
         public float ChartTime;
         private bool isMove;
-        private static float speed = 1.0f;
-        private static double constant = 1.0 / 60.0 / 20.0;
+        
         private float timePerFrame = 0f;
 
         private float suddenOffset = 0.0f;
         private float[] radiusTable;
 
+        private static float speed = 1.0f;
+        private static double constant = 1.0 / 60.0 / 20.0;
+
         private Dictionary<float, RouteSample> routeSamples;
 
-        public static void GetTime(Chart chart)
-        {
-            var bpmChanges = chart.BPMChanges;
-            var lastBPM = bpmChanges[0];
-
-            var notes = chart.Notes;
-            var longNotes = chart.LongNotes;
-            var nextBPMIndex = 1;
-            var oneBeatTime = lastBPM.BPM * constant * speed;
-
-            for (int index = 0; index < notes.Count; index++)
-            {
-                while (nextBPMIndex < bpmChanges.Count)
-                {
-                    if (notes[index].Position > bpmChanges[nextBPMIndex].Position)
-                    {
-                        lastBPM = bpmChanges[nextBPMIndex++];
-                        oneBeatTime = lastBPM.BPM * constant * speed;
-                        continue;
-                    }
-                    break;
-                }
-
-                var fractionValue = 0.0;
-                var position = notes[index].Position - lastBPM.Position;
-
-                if (position.Denominator != 0) fractionValue = position.Numerator / (double)position.Denominator;
-
-                notes[index].Position.Time = lastBPM.Position.Time +
-                    oneBeatTime * (position.Bar + fractionValue);
-            }
-
-            lastBPM = bpmChanges[0];
-            nextBPMIndex = 1;
-            oneBeatTime = lastBPM.BPM * constant * speed;
-
-            for (int longNoteIndex = 0; longNoteIndex < longNotes.Count; longNoteIndex++)
-            {
-                var vertices = longNotes[longNoteIndex].LongNoteVertices;
-                for (int vertexIndex = 0; vertexIndex < vertices.Count; vertexIndex++)
-                {
-                    while (nextBPMIndex < bpmChanges.Count)
-                    {
-                        if (vertices[vertexIndex].Position > bpmChanges[nextBPMIndex].Position)
-                        {
-                            lastBPM = bpmChanges[nextBPMIndex++];
-                            oneBeatTime = lastBPM.BPM * constant * speed;
-                            continue;
-                        }
-                        break;
-                    }
-
-                    var fractionValue = 0.0;
-                    var position = vertices[vertexIndex].Position - lastBPM.Position;
-
-                    if (position.Denominator != 0) fractionValue = position.Numerator / (double)position.Denominator;
-
-                    vertices[vertexIndex].Position.Time = lastBPM.Position.Time +
-                        oneBeatTime * (position.Bar + fractionValue);
-                }
-            }
-        }
 
         private void Awake()
         {
             routeSamples = new Dictionary<float, RouteSample>();
             MakeChartTest();
-            TimeCalculator.CalculateBPMTime(Chart);
-            GetTime(Chart);
+            ChartIntervalCalculator.CalculateBPM(Chart);
+            ChartIntervalCalculator.CalculateChart(Chart);
+
+            ChartTimeCalculator.CalculateBPM(Chart);
 
             TunnelMeshGenerator.GenerateTunnelMesh();
             radiusTable = TunnelMeshGenerator.RadiusTable;
@@ -309,6 +251,11 @@ namespace Beatale.ChartSystem
                 }
                 longNote.TunnelObject.UpdateMesh(longNote.LongNoteMesh);
             }
+        }
+
+        private void RestoreNote(GameObject noteObject)
+        {
+
         }
     }
 }
